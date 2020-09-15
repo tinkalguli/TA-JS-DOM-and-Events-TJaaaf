695114b8ae74dd61de198ff8c802ba98782253b6
tinkalguli <tinkalmunu@gmial.com>
2020-09-15 17:47:14 +0530

    Complete Disco Grid

---
+++ block-BJaael/code/script.js
@@ -0,0 +1,18 @@
+let boxes = document.querySelector(".boxes");
+
+for (let i = 0; i < 500; i++) {
+    let box = document.createElement("div");
+    box.classList.add("box");
+    // box.innerText = num;
+    boxes.append(box);
+}
+
+let allBoxes = document.querySelectorAll(".box");
+
+boxes.addEventListener("mousemove", function () {
+    [...allBoxes].forEach(e => {
+        e.style.backgroundColor = `#${parseInt(Math.random() * 1000)}`;
+        e.innerText = `${parseInt(Math.random() * 1000)}`;
+    });
+});
+
