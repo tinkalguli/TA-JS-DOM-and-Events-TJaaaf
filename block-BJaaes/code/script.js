b2fa07a20f7c7b3c8d2b7d6f05ea2a76d59b152e
tinkalguli <tinkalmunu@gmail.com>
2020-10-04 21:07:10 +0530

    WIP

--- block-BJaaes/code/script.js
+++ block-BJaaes/code/script.js
@@ -1,70 +1,95 @@
 let form = document.querySelector("form");
 
-let userInfo = {};
-
+let username = form.elements.username;
+let name = form.elements.name;
+let email = form.elements.email;
+let phone = form.elements.phone;
+let password = document.querySelector("#password1");
+let passwordCheck = document.querySelector("#password2");
 
 function checkError(inputName) {
   let condition = (function() {
-    if (inputName == form.elements.username) return inputName.value.length < 4;
+    if (inputName == username) return inputName.value.length < 4;
 
-    if (inputName == form.elements.name) return inputName.value.split("").filter(e => !isNaN(e)).length > 0 || userInfo.name == "";
+    if (inputName == name) return inputName.value.split("").some(e => Number(e));
 
-    if (inputName == form.elements.email) return !inputName.value.includes("@") || inputName.value.slice(0, inputName.value.indexOf("@")).length < 6;
+    if (inputName == email) return inputName.value.slice(0, inputName.value.indexOf("@")).length < 6;
 
-    if (inputName == form.elements.phone) return inputName.value.split("").filter(e => isNaN(e)).length > 0 || userInfo.phone == "";
+    if (inputName == phone) return !inputName.value.split("").every(e => Number(e)) || inputName.value.length < 7;
 
-    if (inputName == document.querySelector("#password1")) return (inputName.value.split("").filter(e => { return e.includes("@") || e.includes("$")}).length == 0) 
-    || (userInfo.password.split("").filter(e => !isNaN(e)).length == 0);
+    if (inputName == password) return (!inputName.value.split("").some(e => e == "@" || e == "$"))
+    || (!inputName.value.split("").some(e => Number(e)));
 
-    if (inputName == document.querySelector("#password2")) return (inputName.value.split("").filter(e => { return e.includes("@") || e.includes("$")}).length == 0) 
-    || (userInfo.password.split("").filter(e => !isNaN(e)).length == 0) && userInfo.passwordCheck !== userInfo.password;
-  })();
+    if (inputName == passwordCheck) return passwordCheck.value !== password.value;
+  })();  
+
+  // (inputName.value.split("").some(e => e == "@" || e == "$")) 
+  //   || (inputName.value.split("").some(e => Number(e))) && 
 
   let errorText = (function () {
-    if (inputName == form.elements.username) return "Username can't be less than 4 characters";
+    if (inputName == username) return "Username can't be less than 4 characters";
 
-    if (inputName == form.elements.name) return "You can't use number in the name field";
+    if (inputName == name) return "You can't use number in the name field";
 
-    if (inputName == form.elements.email) return "Not a valid email";
+    if (inputName == email) return "Not a valid email";
 
-    if (inputName == form.elements.phone) return "Phone number can only contain numbers";
+    if (inputName == phone) return "Phone number can only contain numbers";
 
-    if (inputName == document.querySelector("#password1")) return "Password must contain at least a symbol and a number";
+    if (inputName == password) return "Password must contain at least a symbol and a number";
 
-    if (inputName == document.querySelector("#password2")) return "Password not match";
+    if (inputName == passwordCheck) return "Password not match";
   })();
 
-  if (inputName.value.length == 0) {
-    if (inputName.nextElementSibling == null) {
-      let small = document.createElement("small");
-      small.innerText = `${inputName.previousElementSibling.textContent} can not be blank`;
-      inputName.parentElement.append(small);
-      inputName.parentElement.classList.add("error");
-      inputName.parentElement.classList.remove("success");
-    } 
-  } else if (inputName.nextElementSibling) {
-    inputName.nextElementSibling.remove();
-    inputName.parentElement.classList.add("success");
-    inputName.parentElement.classList.remove("error");
-  }
+  // if (inputName.value == "") {
+  //   if (inputName.nextElementSibling == null) {
+  //     let small = document.createElement("small");
+  //     small.innerText = `${inputName.previousElementSibling.textContent} can not be blank`;
+  //     inputName.parentElement.append(small);
+  //     inputName.parentElement.classList.add("error");
+  //     inputName.parentElement.classList.remove("success");
+  //   } 
+  // } else if (inputName.nextElementSibling) {
+  //   inputName.nextElementSibling.remove();
+  //   inputName.parentElement.classList.add("success");
+  //   inputName.parentElement.classList.remove("error");
+  // }
   
-  if (condition) {
-      if (inputName.nextElementSibling == null) {
-        let small = document.createElement("small");
-        small.innerText = errorText;
-        inputName.parentElement.append(small);
-        inputName.parentElement.classList.add("error");
-        inputName.parentElement.classList.remove("success");
-      }
-  } else if (inputName.nextElementSibling) {
-    inputName.nextElementSibling.remove();
-    inputName.parentElement.classList.add("success");
-    inputName.parentElement.classList.add("error");
-  } else inputName.parentElement.classList.add("success");
+  // if (condition) {
+  //     if (inputName.nextElementSibling == null) {
+  //       let small = document.createElement("small");
+  //       small.innerText = errorText;
+  //       inputName.parentElement.append(small);
+  //       inputName.parentElement.classList.add("error");
+  //       inputName.parentElement.classList.remove("success");
+  //     }
+  // } else if (inputName.nextElementSibling) {
+  //   inputName.nextElementSibling.remove();
+  //   inputName.parentElement.classList.add("success");
+  //   inputName.parentElement.classList.add("error");
+  // } else inputName.parentElement.classList.add("success");
+
+  if (inputName.value == "") {
+    console.log("blank");
+    inputName.nextElementSibling.innerText = "";
+    inputName.nextElementSibling.innerText = `${inputName.previousElementSibling.textContent} can not be blank`;
+    inputName.classList.remove("success");
+    inputName.classList.add("error");
+  } else if (condition) {
+    console.log("error");
+    inputName.nextElementSibling.innerText = "";
+    inputName.nextElementSibling.innerText = errorText;
+    inputName.classList.remove("success");
+    inputName.classList.add("error");
+  } else {
+    console.log("success")
+    inputName.nextElementSibling.innerText = "";
+    inputName.classList.remove("error");
+    inputName.classList.add("success");
+  }
 }
 
-function validSubmission () {
-  let allFormControl = document.querySelectorAll(".form-control");
+function validSubmission() {
+  let allFormControl = document.querySelectorAll("input");
   let isValid = [...allFormControl].every(e => e.classList.contains("success"));
   if (isValid) {
     let validHeading = document.createElement("h2");
@@ -90,27 +115,16 @@ function validSubmission () {
   }
 }
 
-
 function handleSubmit(event) {
   event.preventDefault();
-  userInfo.username = form.elements.username.value;
-  userInfo.name = form.elements.name.value;
-  userInfo.email = form.elements.email.value;
-  userInfo.phone = form.elements.phone.value;
-  userInfo.password = document.querySelector("#password1").value;
-  userInfo.passwordCheck = document.querySelector("#password2").value;
 
-  checkError(form.elements.username);
-  checkError(form.elements.name);
-  checkError(form.elements.email);
-  checkError(form.elements.phone);
-  checkError(document.querySelector("#password1"));
-  checkError(document.querySelector("#password2"));
+  checkError(username);
+  checkError(name);
+  checkError(email);
+  checkError(phone);
+  checkError(password);
+  checkError(passwordCheck);
   validSubmission();
-
-  // send data to server
-  console.dir(userInfo);
 }
-
 
 form.addEventListener("submit", handleSubmit);
